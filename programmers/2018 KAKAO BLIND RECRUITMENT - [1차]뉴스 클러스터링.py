88bee831c9eafb1b022dcf6983bd84fcc369d405
gyounghee <ghl0209@naver.com>
2022-04-29 19:02:06 +0900

    다른 사람 풀이 분석 후 내 방식대로 수정

--- programmers/2018 KAKAO BLIND RECRUITMENT - [1차]뉴스 클러스터링.py
+++ programmers/2018 KAKAO BLIND RECRUITMENT - [1차]뉴스 클러스터링.py
@@ -73,14 +73,12 @@ print(solution(str1,str2))     # 32768
 
 
 
-## 다른 사람 풀이
-
+## 다른 사람 풀이 해석 후 내 방식대로 변형
 import re
-import math
 
 def solution(str1, str2):
-    str1 = [str1[i:i+2].lower() for i in range(0, len(str1)-1) if not re.findall('[^a-zA-Z]+', str1[i:i+2])]
-    str2 = [str2[i:i+2].lower() for i in range(0, len(str2)-1) if not re.findall('[^a-zA-Z]+', str2[i:i+2])]
+    str1 = [str1[i:i+2].lower() for i in range(0, len(str1)-1) if re.findall('[a-zA-Z]+[a-zA-Z]', str1[i:i+2])]
+    str2 = [str2[i:i+2].lower() for i in range(0, len(str2)-1) if re.findall('[a-zA-Z]+[a-zA-Z]', str2[i:i+2])]
 
     gyo = set(str1) & set(str2)
     hap = set(str1) | set(str2)
@@ -91,4 +89,4 @@ def solution(str1, str2):
     gyo_sum = sum([min(str1.count(gg), str2.count(gg)) for gg in gyo])
     hap_sum = sum([max(str1.count(hh), str2.count(hh)) for hh in hap])
 
-    return math.floor((gyo_sum/hap_sum)*65536)
+    return int(gyo_sum / hap_sum * 65536)
