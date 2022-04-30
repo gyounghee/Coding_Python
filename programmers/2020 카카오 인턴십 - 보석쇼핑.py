849da975fcd81e6f9c1d8ab95f315757f73cfa31
gyounghee <ghl0209@naver.com>
2022-05-01 01:43:17 +0900

    클론 코딩 & 코드 분석 - 추후 꼭 다시 풀기

--- programmers/2020 카카오 인턴십 - 보석쇼핑.py
+++ programmers/2020 카카오 인턴십 - 보석쇼핑.py
@@ -1,20 +1,31 @@
-### 푸는 
+### 클론 코딩 & 코드 분석 함 - 프로그래머스 제출 X (나중에 꼭 다시 풀어보기)
 def solution(gems):
-    answer = []
-    f_gems = list(set(gems))   # 찾고자 하는 보석
-
-    # 보석 data의 정보를 dict형식으로
-    gem_dict = dict.fromkeys(gems)
-    for gem in f_gems :
-        gem_dict[gem] = gems.count(gem)
-
-
-    # 보석 구간 찾기
-    if len(gem_dict.keys()) == 1 :  # 보석의 종류가 1개인 경우
+    find = len(set(gems))   # 찾고자 하는 보석
+    if find == 1 :  # 보석의 종류가 1개인 경우 
         return [1,1]
-    elif len(gem_dict.keys()) == sum(gem_dict.values()):  # 여러 보석이 각각 1개 씩 있는 경우
-        return [1,len(gems)]
-    
+
+    left, right = 0,1
+    answer = [1,len(gems)]
+    data = dict()
+    data[gems[left]] = 1
+    before = 0
+    while left < right :
+        try :
+            if before != right :
+                data[gems[right]] += 1
+                before = right
+        except KeyError :
+            data[gems[right]] = 1
+            before = right
+        if len(data) == find and right-left < answer[1] - answer[0] :  # 만약 data의 길이와 찾으려는 길이가 같고, ~~ 면
+            answer = [left+1, right+1]
+        if right < len(gems)-1 and len(data) != find :
+            right += 1
+        else :
+            data[gems[left]] -= 1
+            if data[gems[left]] == 0 :
+                del data[gems[left]]
+            left += 1
     return answer
 
 
@@ -26,10 +37,18 @@ print(solution(gems))   # [3, 7]
 gems = ["AA", "AB", "AC", "AA", "AC"]
 print(solution(gems))   # [1, 3]
 
-# TEST CASE Ⅱ
+# TEST CASE Ⅲ
 gems = ["XYZ", "XYZ", "XYZ"]
 print(solution(gems))   # [1, 1]
 
-# TEST CASE Ⅱ
+# TEST CASE Ⅳ
 gems = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
 print(solution(gems))   # [1, 5]
+
+# TEST CASE Ⅴ
+gems = ["A","A","A","B","B"] 
+print(solution(gems))   # [3,4]
+
+# TEST CASE Ⅵ
+gems = ["A","B","B","B","B","B","B","C","B","A"]  
+print(solution(gems))   # [8,10]
