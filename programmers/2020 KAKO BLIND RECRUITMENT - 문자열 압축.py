df439d9c1c8c75cfbf0ee9f9d4013fda4187209a
gyounghee <ghl0209@naver.com>
2022-05-03 00:10:27 +0900

    다른 풀이 코드 분석

--- programmers/2020 KAKO BLIND RECRUITMENT - 문자열 압축.py
+++ programmers/2020 KAKO BLIND RECRUITMENT - 문자열 압축.py
@@ -19,13 +19,7 @@ def s_zip(s,zl):
             return short
         
 def solution(s):
-    zip_len = 9999
-    for zl in range(1, len(s)+1): # 압축하려는 문자열을 하나씩 늘려가면서 
-        tmp = len(s_zip(s, zl))
-        if tmp <= zip_len :
-            zip_len = tmp
-    return zip_len
-
+    return min(len(s_zip(s,zl)) for zl in range(1,len(s)+1))  # 압축할 문자 길이를 하나씩 늘려가며
 
 
 # TEST CASE Ⅰ
@@ -51,3 +45,23 @@ print(solution(s))   # 17
 # TEST CASE Ⅵ
 s = "aaaaaaaaaa" 
 print(solution(s))   # 3
+
+
+## 다른 사람 풀이 
+def compress(text, tok_len):
+    words = [text[i:i+tok_len] for i in range(0, len(text), tok_len)]
+    res = []
+    cur_word = words[0]
+    cur_cnt = 1
+    for a, b in zip(words, words[1:] + ['']):
+        if a == b:
+            cur_cnt += 1
+        else:
+            res.append([cur_word, cur_cnt])
+            cur_word = b
+            cur_cnt = 1
+    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res)
+
+def solution(text):
+    return min(compress(text, tok_len) for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)])
+                                                    # 압축하려면 최소 같은 문자열이 2개 이상있어야 하므로 text길이에서 2를 나눠준 것 (내 풀이처럼 하나씩 다 늘려가며 할 필요 없음)
