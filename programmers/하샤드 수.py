cc15c20a825c86e4cef7c9cad0d6ca36eee35ff6
gyounghee <ghl0209@naver.com>
2022-05-11 00:01:40 +0900

    하샤드 수 - 다른 풀이 분석

--- programmers/하샤드 수.py
+++ programmers/하샤드 수.py
@@ -1,3 +1,21 @@
 def solution(x):
-    n_total = sum(list(map(int, list(str(x)))))
-    return True if x % n_total == 0 else False
+    x_sum = sum(list(map(int, list(str(x)))))
+    return True if x % x_sum == 0 else False
+
+# TEST CASE 
+arr = 10
+print(solution(arr))   # true
+arr = 12
+print(solution(arr))   # true
+arr = 11
+print(solution(arr))   # false
+arr = 13
+print(solution(arr))   # false
+
+
+
+
+## 다른 사람 풀이
+# str과 for문 & 비교 연산자 사용 
+def solution(x):
+    return x % sum([int(c) for c in str(x)]) == 0
