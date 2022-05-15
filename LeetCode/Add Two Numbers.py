2763f05fac8f0802bc179a06ff672a21ab19ff81
gyounghee <ghl0209@naver.com>
2022-05-15 23:26:01 +0900

    문제 푸는거 쉬고, 책으로 알고리즘 공부함

--- LeetCode/Add Two Numbers.py
+++ LeetCode/Add Two Numbers.py
@@ -3,4 +3,4 @@ class Solution:
         l1_n = int(''.join(list(map(str, l1[::-1]))))
         l2_n = int(''.join(list(map(str, l2[::-1]))))
         n_sum = list(str(l1_n+l2_n))
-        return list(map(int, n_sum))[::-1] 
+        return list(map(int, n_sum))[::-1]  
