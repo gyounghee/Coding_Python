2c0ad9de1d84d557d38480fc8ce40e0786ddbc9f
gyounghee <ghl0209@naver.com>
2022-05-18 17:03:05 +0900

    stack을 이용한 풀이법 추가

--- LeetCode/42. Trapping Rain Water.py
+++ LeetCode/42. Trapping Rain Water.py
@@ -23,3 +23,26 @@ class Solution:
                 water += right_max - height[right]
                 right -= 1
         return water
+
+
+#### 책 풀이 - 2. 스택 쌓기
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack, volume = [], 0
+        
+        for i in range(len(height)):
+            # 변곡점을 만나는 경우
+            while stack and height[i] > height[stack[-1]] :
+                top = stack.pop()
+                
+                if not len(stack) :
+                    break
+                    
+                # 이전과의 차이만큼 물 높이 처리
+                distance = i - stack[-1] - 1
+                waters  = min(height[i], height[stack[-1]]) - height[top]
+                volume += distance * waters
+            
+            stack.append(i)
+        return volume
+            
