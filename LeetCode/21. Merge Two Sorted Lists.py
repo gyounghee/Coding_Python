20a341d93f566287c799936143eb3e082b340c74
gyounghee <ghl0209@naver.com>
2022-05-23 23:11:42 +0900

    코드 분석 ( 다시 풀어보기 )

--- LeetCode/21. Merge Two Sorted Lists.py
+++ LeetCode/21. Merge Two Sorted Lists.py
@@ -9,12 +9,12 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # list1이 존재하지 않거나 또는 list2가 존재하면서 list1의 첫 번쨰 값이 list2의 첫 번째 값보다 크면
         if (not list1) or (list2 and list1.val > list2.val) :
-            list1, list2 = list2, list1
+            list1, list2 = list2, list1    # 항상 list1에 작은 값이 들어가도록 swap(스왑)
         
-        if list1 : 
+        if list1 :  # list1이 존재하면 
+            # list1.next는 list1.next의 다음 원소와 list2의 값을 파라미터로 하여 재귀
             list1.next = self.mergeTwoLists(list1.next, list2)
         
-        return list1
-
-
+        return list1  
