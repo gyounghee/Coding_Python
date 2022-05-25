a39a58e31efe67baa80173c750341fb0820441f2
gyounghee <ghl0209@naver.com>
2022-05-25 13:52:29 +0900

    다른 풀이 코드 분석

--- programmers/Summer, Winter Coding(~2018) - 영어 끝말잇기.py
+++ programmers/Summer, Winter Coding(~2018) - 영어 끝말잇기.py
@@ -14,3 +14,12 @@ def solution(n, words):
             return [p_num, order]
     return [0,0]
 
+
+# 다른 사람 풀이 - in을 사용
+
+def solution(n, words):
+    for i in range(1, len(words)):
+        if words[i][0] != words[i-1][-1]  or words[i] in words[:i] :
+            return [(i%n)+1, (i//n)+1]
+    else:
+        return [0,0]
