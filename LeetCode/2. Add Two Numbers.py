46f8b3bc2eba458a650addf10b059a954bc6f6f3
gyounghee <ghl0209@naver.com>
2022-05-25 17:22:47 +0900

    코드 분석 해야함

--- LeetCode/2. Add Two Numbers.py
+++ LeetCode/2. Add Two Numbers.py
@@ -74,3 +74,32 @@ class Solution:
             
             # 최종 결과 연결리스트 변환
             return self.toReverseLinkedList(str(resultStr))
+
+
+#### 책 풀이 - 2. 전가산기 구현
+        class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        root = head = ListNode(0)
+        
+        carry = 0 
+        while l1 or l2 or carry :
+            sum = 0
+            # 두 입력값의 합 계산
+            if l1 :
+                sum += l1.val
+                l1 = l1.next
+            if l2 :
+                sum += l2.val
+                l2 = l2.next
+            
+            # 몫(자리올림수)과 나머지(값) 계산
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+            
+        return root.next
+    
