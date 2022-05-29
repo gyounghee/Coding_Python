df4b5ca7dcf2fc081d2fcee25c5da464f442cf98
gyounghee <ghl0209@naver.com>
2022-05-30 01:21:52 +0900

    풀이 중..

--- programmers/## 가장 큰 수 (정렬).py
+++ programmers/## 가장 큰 수 (정렬).py
@@ -1,4 +1,6 @@
-# TEST CASE  1 ~ 6 실패
+# TEST CASE  1 ~ 6 실패 - 시간초과
+#  문제인지 : permutations을 사용 - 비효율적인 코드
+
 def find_max(num_list):
     from itertools import permutations
     p_list = list(permutations(num_list,len(num_list)))
@@ -10,8 +12,7 @@ def solution(numbers):
         return "0"
     
     answer = ''
-    str_n = list(map(str, numbers))
-    sort_n = sorted(str_n, key = lambda x:x[0], reverse = True)
+    sort_n = sorted(list(map(str, numbers)), key = lambda x:x[0], reverse = True)
     
     same = 0
     for i in range(len(sort_n)-1):
@@ -19,15 +20,11 @@ def solution(numbers):
             answer += sort_n[i]
         elif  sort_n[i][0] == sort_n[i+1][0] :
             same += 1
-        elif same != 0 and sort_n[i][0] != sort_n[i+1][0] :
+        else :   # same != 0 and sort_n[i][0] != sort_n[i+1][0] 
             answer += find_max(sort_n[i-same:i+1])
             same = 0
     
-    if same :
-        answer += find_max(sort_n[-same-1:])
-    else :
-        answer += sort_n[-1]
-            
+    answer += find_max(sort_n[-same-1:]) if same else sort_n[-1]
     return answer
 
 
@@ -49,3 +46,49 @@ print(solution(numbers))
 
 
 
+
+### 두 번째 풀이 - permutations() 사용하지 않고 풀이
+def find_max(num_list):
+    len_c = [[],[],[],[]]    # 길이별로 분류하기 위한 list
+    for n in num_list:   # 길이별로 분류하여 list에 저장
+        len_c[len(n)-1].append(n)
+        
+    sort_nums = sum( [sorted(n, reverse=True) for n in len_c], [] )
+    return ''.join(sort_nums)
+    
+def solution(numbers):
+    if sum(numbers) == 0 :
+        return "0"
+    
+    answer = ''
+    sort_n = sorted(list(map(str, numbers)), key = lambda x:x[0], reverse = True)
+    
+    same = 0
+    for i in range(len(sort_n)-1):
+        if (sort_n[i] == '0') or ( same == 0 and sort_n[i][0] != sort_n[i+1][0] ) :
+            answer += sort_n[i]
+        elif  sort_n[i][0] == sort_n[i+1][0] :
+            same += 1
+        else :   # same != 0 and sort_n[i][0] != sort_n[i+1][0] 
+            answer += find_max(sort_n[i-same:i+1])
+            same = 0
+    
+    answer += find_max(sort_n[-same-1:]) if same else sort_n[-1]
+    return answer
+
+
+
+### 세 번째 풀이 - permutations() 사용하지 않고 풀이
+def solution(numbers):
+    if sum(numbers) == 0 :
+        return "0"
+    
+    str_n = list(map(str, numbers))
+    
+    len_c = [[],[],[],[]]    # 길이별로 분류하기 위한 list
+    for n in str_n:   # 길이별로 분류하여 list에 저장
+        len_c[len(n)-1].append(n)
+    
+    sort_nums = sum( [sorted(n, reverse=True) for n in len_c], [] )
+    
+    return ''.join(sort_nums)
