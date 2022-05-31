8a480ce1c05df5a7bea52b49f9d441249fef473c
gyounghee <ghl0209@naver.com>
2022-06-01 00:53:00 +0900

    예상 대진표 - 코드 분석

--- programmers/2017 팁스타운 - 예상 대진표.py
+++ programmers/2017 팁스타운 - 예상 대진표.py
@@ -4,8 +4,17 @@ def solution(n,a,b):
     a,b = min(a,b), max(a,b)
     
     while abs(a-b) != 1 or b %2 == 1:
-        if a != 1 :
-            a = a//2 if a % 2 == 0 else (a//2)+1 
+        a = a//2 if a % 2 == 0 else (a//2)+1 
         b = b//2 if b % 2 == 0 else (b//2)+1     
         answer += 1
+    return answer
+
+
+# 다른 사람 풀이 
+def solution(n,a,b):
+    answer = 0
+    while a != b:
+        answer += 1
+        a, b = (a+1)//2, (b+1)//2
+
     return answer
