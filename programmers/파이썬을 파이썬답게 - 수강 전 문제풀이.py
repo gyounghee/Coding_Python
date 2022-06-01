ee0d03d520a16d9a5e5c6610d96a65ea85eb45d4
gyounghee <ghl0209@naver.com>
2022-06-01 10:06:08 +0900

    pythonic 한 코드 추가

---
+++ programmers/파이썬을 파이썬답게 - 수강 전 문제풀이.py
@@ -0,0 +1,11 @@
+# 프로그래밍 강의
+# - 파이썬을 파이썬답게 - 수강 전 이 문제를 풀어보세요.
+
+def solution(mylist):
+    return [len(lst) for lst in mylist]
+
+
+
+### 강의 풀이 - 좀 더 파이써닉한 코드
+def solution(mylist):
+    return list(map(len, mylist))
