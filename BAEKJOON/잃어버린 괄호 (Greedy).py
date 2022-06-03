705f9d279b0edc54423a527b67f096b779b71a93
gyounghee <ghl0209@naver.com>
2022-06-03 20:47:58 +0900

    코드 개선 및 다른 풀이 코드 분석

--- BAEKJOON/잃어버린 괄호 (Greedy).py
+++ BAEKJOON/잃어버린 괄호 (Greedy).py
@@ -1,3 +1,42 @@
+## 풀이 2 - 풀이1 개선
+# - 수행시간은 풀이 1과 동일
+f = input()
+P = 0     # 괄호(Parenthesis)
+add_n, minus_n  = '', ''
+
+for i in range(len(f)) :
+    if not P and f[i] == '-' :  # 괄호가 닫힌 상태에서 '-' 만나면 
+        P = 1                   # 괄호를 열음
+    elif P and f[i] == '-' :    # 괄호가 열린 상태에서 '-' 만나면
+        minus_n += '+'
+    elif P  :                   # 괄호가 열려있는 경우
+        minus_n += f[i]         #  minus_n에 추가
+    else :
+        add_n += f[i]           # add_n에 추가
+
+minus_n = sum( list(map(int, minus_n.split('+'))) ) if len(minus_n) else 0
+add_n =  sum( list(map(int, add_n.split('+'))) ) if len(add_n) else 0
+print(add_n - minus_n)
+
+
+
+## 다른 사람풀이
+# - 입력 받은 것을 먼저 '-'를 구분자로 하여 나눈 후 거기서 또 '+'를 구분자로 하여 나눈다.
+# - 그럼 첫 번째 원소만 +인자를 가진 원소가 될 것이기 때문에
+# - 첫 번째 원소에서 첫 번째 원소 이후 모든 값의 합을 빼주면 최소값이 됨
+e = [sum(map(int, x.split('+'))) for x in input().split('-')] 
+print(e[0]-sum(e[1:]))
+
+
+
+
+
+
+
+
+
+## 풀이 1
+# - 코드가 매우 더러워서 창피하다
 f = input()
 
 open_P = 0     # 괄호(Parenthesis)
