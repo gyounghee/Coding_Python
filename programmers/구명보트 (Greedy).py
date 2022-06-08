096362031dcfc76e017d5b43917805b8a57e7838
gyounghee <ghl0209@naver.com>
2022-06-08 11:00:00 +0900

    다른 사람 풀이 분석

--- programmers/구명보트 (Greedy).py
+++ programmers/구명보트 (Greedy).py
@@ -1,5 +1,6 @@
 # 구명보트 (Greedy)
 
+# 12m
 from collections import deque
 
 def solution(people, limit):
@@ -16,4 +17,18 @@ def solution(people, limit):
     return answer if not p else answer + 1
 
 
-# 12m
+### 다른 사람 풀이
+# - 투 포인터를 이용한 풀이
+# - 짝 지었을때만 2명씩 나가기 때문에 전체 인원에서 짝 지은 수만 빼주면 보트의 수가 나오는 것을 이용
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
