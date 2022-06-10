6178ac7bacf455e5056f73fe09f2eeca9536a082
gyounghee <ghl0209@naver.com>
2022-06-11 00:00:20 +0900

    .

--- BAEKJOON/뒤집기.py
+++ BAEKJOON/뒤집기.py
@@ -9,3 +9,13 @@ s = input()
 
 s0, s1 = s.split('0'), s.split('1')
 print( min(check(s0), check(s1)) )
+
+
+## 다른 사람 풀이
+change = 0
+prev = '?'
+string = input()
+for i in string:
+    if i != prev : change += 1
+    prev = i
+print(change//2)
