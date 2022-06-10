80501fef5ec8720b9feccdd6e3f385fa2a28553c
gyounghee <ghl0209@naver.com>
2022-06-10 23:04:33 +0900

    heap 관련 코드 주석 추가

--- BAEKJOON/카드 정렬하기.py
+++ BAEKJOON/카드 정렬하기.py
@@ -4,7 +4,7 @@ from sys import stdin
 from heapq import * 
 
 numbers = [ int(stdin.readline()) for _ in range(int(stdin.readline()))]
-heapify(numbers)
+heapify(numbers) # heapify()에 리스트를 인자로 넘기면 리스트 내부의 원소들의 위에서 다룬 힙 구조에 맞게 재배치되며 최소값이 0번째 인덱스에 위치됨
 total = 0
 
 for _ in range(len(numbers)-1) :
