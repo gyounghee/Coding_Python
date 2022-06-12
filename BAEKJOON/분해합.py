9b0ac00ed1c7479d0606279ea0ae1e13b194cc0b
gyounghee <ghl0209@naver.com>
2022-06-12 21:35:37 +0900

    다른 풀이 추가

--- BAEKJOON/분해합.py
+++ BAEKJOON/분해합.py
@@ -1,12 +1,27 @@
+# 1092번 - 분해
+
+# 시간 : 676ms
 from sys import stdin
 
 n = int(stdin.readline())
 find = 0
 
 for i in range(n//2, n):
-     tmp = list(map(int, list(str(i))))
-     if i + sum(tmp) == n :
+     if i + sum(map(int,str(n))) == n :
          find = i
          break
-
 print(find)
+
+
+
+## 다른 사람 풀이
+# 시간 : 68ms 
+
+N=int(input())
+r=0
+
+for i in range(max(0, N-100), N):
+    if sum(map(int,list(str(i))))+i==N:
+        r=i
+        break
+print(r)
