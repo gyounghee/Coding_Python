94ce63cb2f052ba21bd49b813a1bb37c5c3f33b5
gyounghee <ghl0209@naver.com>
2022-06-13 15:48:05 +0900

    다른 풀이 추가

---
+++ BAEKJOON/최대공약수와 최소공배수.py
@@ -0,0 +1,21 @@
+# 2609번 - 최대공약수와 최소공배수
+
+# 풀이 1
+# math의 gcd,lcm 라이브러리 사용
+from math import gcd,lcm
+
+a, b = map(int, input().split())
+print(gcd(a,b))
+print(lcm(a,b))
+
+
+## 풀이 2
+# - 최소 공배수 = 두 수의 곱 / 최대공약수
+x, y = map(int,input().split())
+a, b = max(x,y), min(x,y)
+
+while b != 0:
+    a,b = b,a%b
+    
+print(a)
+print(x*y//a)
