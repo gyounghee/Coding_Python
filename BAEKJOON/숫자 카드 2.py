6a0a3f869759e683d9a8045c1fbae3e440a1a01e
gyounghee <ghl0209@naver.com>
2022-06-15 10:26:10 +0900

    다른 풀이 추가

--- BAEKJOON/숫자 카드 2.py
+++ BAEKJOON/숫자 카드 2.py
@@ -1,5 +1,6 @@
 # 10816번 - 숫자 카드 2
 
+## 풀이 1
 from sys import stdin
 from collections import defaultdict
 
@@ -9,6 +10,19 @@ M, find = int(stdin.readline()), list(map(int, stdin.readline().split()))
 num_dict = defaultdict(int)
 for n in num :
     num_dict[n] += 1
+
+for f in find :
+    print(num_dict[f],end=' ')
+
+
+## 풀이 2
+from sys import stdin
+from collections import Counter
+
+N, num = int(stdin.readline()), list(map(int, stdin.readline().split()))
+M, find = int(stdin.readline()), list(map(int, stdin.readline().split()))
+
+num_dict = Counter(num)
 
 for f in find :
     print(num_dict[f],end=' ')
