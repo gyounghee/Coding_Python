83f849c6392aa25d13b13b4c2950f0a91f759f93
gyounghee <ghl0209@naver.com>
2022-06-17 14:37:09 +0900

    [update] source code

--- BAEKJOON/소수 구하기.py
+++ BAEKJOON/소수 구하기.py
@@ -4,6 +4,7 @@ N, M = map(int, input().split())
 
 for n in range(N,M+1):
     if n == 1 : continue
+    if n == 2 : print(2)
     
     sosu, sqrt = 1, int(n**(1/2))+1
     for i in range(2,sqrt+1):
@@ -12,3 +13,4 @@ for n in range(N,M+1):
             break
     if sosu == 1 :
         print(n)
+
