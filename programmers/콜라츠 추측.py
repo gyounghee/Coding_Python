f90e059c8aade28b1d6c39c9a72366aebda8599d
gyounghee <ghl0209@naver.com>
2022-06-19 11:49:45 +0900

    [Add] source code

--- programmers/콜라츠 추측.py
+++ programmers/콜라츠 추측.py
@@ -1,11 +1,21 @@
 # 콜라츠 추측
 
+# 풀이 1
 def solution(num) :
     answer = 0
-    if num == 1 : return 0
-
     while num > 1 :
         if answer == 500 : return -1
         num = num//2 if num  % 2 == 0 else num*3+1
         answer += 1
     return answer
+
+
+
+# 풀이 2 :
+def solution(num) :
+    for i in range(500) :
+        if num == 1 : 
+            return i
+        else :
+            num = num//2 if num  % 2 == 0 else num*3+1
+    return -1
