13fa7ef848d666dbcbcb039f475c8ef25ee598c7
gyounghee <ghl0209@naver.com>
2022-06-26 22:38:33 +0900

    [update] source code

--- BAEKJOON/덩치.py
+++ BAEKJOON/덩치.py
@@ -4,17 +4,32 @@ from sys import *
 input = stdin.readline
 
 N = int(input())
-student = [ list(map(int, input().split())) for i in range(N) ]
-student = [ [i] + v for i, v in enumerate(student) ]
-student.sort(key = lambda x : (x[1], x[2]), reverse=True)
+s = [ list(map(int, input().split())) for i in range(N) ]
+s = [ [i] + v for i, v in enumerate(s) ]
+s.sort(key = lambda x : (x[1], x[2]), reverse=True)
 
-student[0] = [student[0][0], student[0][1], student[0][2], 1]
-for i in range(1,N) :
+for i in range(N) :
     tmp = i+1
     for j in range(i-1,-1,-1) :
-        if student[i][1] == student[j][1] or student[i][2] >= student[j][2] :
+        if s[i][1] == s[j][1] or s[i][2] >= s[j][2] :
             tmp -= 1
-    student[i] = [student[i][0], student[i][1], student[i][2], tmp]
+    s[i] = [s[i][0], s[i][1], s[i][2], tmp]
 
-seq = sum(sorted(student, key=lambda x : x[0]), [])
-print( ' '.join(list(map(str, seq[3::4]))) )
+seq = sum(sorted(s, key=lambda x : x[0]), [])
+print( ' '.join(map(str, seq[3::4])) )
+
+
+
+## 다른 사람 풀이
+N= int(input())
+
+S = [list(map(int, input().split())) for i in range(N)]
+
+for x1, y1 in S:
+    result = 1
+    for x2, y2 in S:
+        if x1 < x2 and y1 < y2:
+            result += 1
+
+
+    print(result, end =' ')
