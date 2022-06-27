ebceb1fe0c322979999e0b18f553e0b77ae29b50
gyounghee <ghl0209@naver.com>
2022-06-27 10:13:28 +0900

    [add] 다른 사람 풀이

--- BAEKJOON/A → B.py
+++ BAEKJOON/A → B.py
@@ -25,3 +25,23 @@ print(ans if M in stack else '-1')
 
 
 
+### 다른 사람 풀이
+
+n,m = map(int,input().split())
+
+count=0
+while n!=m:
+    if n > m:
+        count = -2
+        break
+    elif str(m)[-1]=='1':
+        m = m//10
+        count += 1
+    elif m%2 == 0:
+        m = m//2
+        count += 1
+    else:
+        count = -2
+        break
+    
+print(count+1)
