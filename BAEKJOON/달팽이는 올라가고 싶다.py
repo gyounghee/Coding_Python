8b08c15ec97965285cfd2b48d81e97e1ddd8c4cb
gyounghee <ghl0209@naver.com>
2022-06-28 21:47:00 +0900

    [add] source code

--- BAEKJOON/달팽이는 올라가고 싶다.py
+++ BAEKJOON/달팽이는 올라가고 싶다.py
@@ -1,5 +1,6 @@
 # 2869번 - 달팽이는 올라가고 싶다
 
+## 풀이 1
 from sys import stdin
 from math import ceil  # 올
 
@@ -7,3 +8,8 @@ A, B, V = map(int, stdin.readline().split())
 need = V - A
 print( ceil( need/(A-B) ) + 1 )
 
+
+## 풀이 2
+A, B, V = map(int, input().split())
+
+print( (V-A)//(A-B) + 1 if (V-A)%(A-B) == 0 else (V-A)//(A-B) + 2 )
