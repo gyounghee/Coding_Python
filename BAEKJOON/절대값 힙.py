f2d7fa5bf77b86b4f048c84ddd20eee15bc66ba2
gyounghee <ghl0209@naver.com>
2022-07-03 02:17:15 +0900

    [add] source code

--- BAEKJOON/절대값 힙.py
+++ BAEKJOON/절대값 힙.py
@@ -1,6 +1,6 @@
 # 11286번 - 절대값 힙 
 
-## 내 풀이
+## 풀이 1
 # - 메모리 : 34176 KB  /  시간 : 160 ms
 
 from heapq import *
@@ -31,3 +31,26 @@ for _ in range(int(input())) :
         else :
             print(pn)
             heappush(m, mn)
+
+
+
+## 풀이 2
+# - 메모리 : 34176 KB  /  시간 : 144 ms
+
+from heapq import *
+from sys import *
+input = stdin.readline
+
+p, m = [], []
+
+for _ in range(int(input())) :
+    n = int(input())
+    if n < 0 : heappush(m, -n)
+    elif n > 0 : heappush(p, n)
+    else :
+        if p and m :
+            if m[0] <= p[0] : print(-heappop(m))
+            else : print(heappop(p))
+        elif p : print(heappop(p))
+        elif m : print(-heappop(m))
+        else : print(0)
