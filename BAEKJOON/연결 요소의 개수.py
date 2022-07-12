cab219e3dad632d009fbb4d02186523d712576bd
gyounghee <ghl0209@naver.com>
2022-07-12 10:23:53 +0900

    220712_two ( BFS 풀이 )

--- BAEKJOON/연결 요소의 개수.py
+++ BAEKJOON/연결 요소의 개수.py
@@ -27,3 +27,36 @@ for i in range(1,N+1):
         ans += 1
         
 print(ans)
+
+
+
+## 풀이 2
+# - BFS 이용
+# - 메모리 : 64516 KB  /  시간 : 2640 ms
+
+from collections import deque
+from sys import *
+input = stdin.readline
+
+def BFS(i) :
+    q = deque([i])
+    while q :
+        v = q.popleft()
+        visited[v] = True
+        for n in V[v]:
+            if not visited[n] and n not in q : q.append(n)
+
+N, M = map(int, input().split())
+V = [[] for _ in range(N+1)]
+for _ in range(M):
+    i, j = map(int, input().split())
+    V[i].append(j); V[j].append(i) 
+
+visited = [False]*(N+1)
+ans = 0
+for i in range(1,N+1):
+    if not visited[i] :
+        BFS(i)
+        ans += 1
+        
+print(ans)
